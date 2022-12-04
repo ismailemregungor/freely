cbc86ad51cb59d529925f53aa322be2ebcf80ddd
İsmail Emre Güngör <ismailemre.gungor@gazi.edu.tr>
2022-12-04 23:53:01 +0300

    update Home component

--- freely-frontend/src/container/Home.jsx
+++ freely-frontend/src/container/Home.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import UserProfile from "../components/UserProfile";
+import Pins from "../container/Pins";
+import { client } from "../client";
+import logo from "../assets/logo.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [ToggleSidebar, setToggleSidebar] = useState(false);
+
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
+        <Sidebar />
+      </div>
+      <div className="flex md:hidden flex-row">
+        <HiMenu
+          fontSize={40}
+          className="cursor-pointer"
+          onClick={() => setToggleSidebar(true)}
+        />
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
